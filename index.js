068e38a911143b1ed12c35d37367d28aa9a3ad42
Jingchao Yu <jingchaoyu@hotmail.com>
2020-12-03 22:06:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-// add solution here
+const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+
+const playlists = [];
+
+function theBeatlesPlay(musicians, instruments) {
+  for (let i = 0; i < musicians.length; i++) {
+    let song = `${musicians[i]} plays ${instruments[i]}`;
+    playlists.push(song);
+  }
+  return playlists;
+}
+
+// Second problem
+const facts = [
+    "He was the last Beatle to learn to drive",
+    "He was never a vegetarian",
+    "He was a choir boy and boy scout",
+    "He hated the sound of his own voice"
+];
+
+const newFacts = [];
+
+function johnLennonFacts(facts) {
+    let i = 0;
+    let fact;
+    while (i < facts.length) {
+        fact = `${facts[i]}!!!`;
+        i++;
+        newFacts.push(fact);
+    } 
+    return newFacts;
+  }
+
+
+  //Third problem
+  let testaments = [];
+  function iLovetheBeatles(num) {
+    let i = 15 - num;
+    do {
+      let testament = "I love the Beatles!\n";
+      testaments.push(testament);
+      i++;
+    } 
+    while (i < 15);
+  }
+
+iLovetheBeatles(10);
